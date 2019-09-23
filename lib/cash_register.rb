ca6e1229f86a873ce41a3eba6ca7446017a59aae
Cha Yang <chachaiyang3@gmail.com>
2019-09-23 16:33:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
+  attr_accessor :total, :discount, 
+  
+  def initialize(discount = 0, total = 0)
+    @discount = discount
+    @total = total
+    @total_after_discount = []
+    
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price)
+    self.total += price
+  end
+end
+
 
