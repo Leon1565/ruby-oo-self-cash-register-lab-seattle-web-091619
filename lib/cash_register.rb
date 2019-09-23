6518fc6cb423669f60c64d467f8ccc8ea429cde5
Cha Yang <chachaiyang3@gmail.com>
2019-09-23 16:55:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   def add_item(title, price, quantity = nil)
     @quantity = quantity
     self.total += price
-    
+    transaction = price * quantity
   end
   
   def apply_discount
